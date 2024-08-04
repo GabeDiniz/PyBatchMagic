740b2e4e2df19dc3e7ba9258009be4d25d3b9f33
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-08-04 11:40:59 -0400

    [update] git-catchup to pull from multiple repos

--- bin/git-catchup.bat
+++ bin/git-catchup.bat
@@ -1,13 +1,19 @@
 @echo off
 setlocal
 
-:: Set path to your local repo here
+:: Set path to your local repo(s) here
+:: REPO 1
 set repoDir=C:\path\to\your\repo
 echo Navigating to repository directory...
 cd %repoDir%
-
 echo Pulling latest changes...
 git pull origin main
-
 echo Pull complete!
-pause
+
+:: REPO 2
+set repoDir=C:\path\to\your\repo
+echo Navigating to repository directory...
+cd %repoDir%
+echo Pulling latest changes...
+git pull origin main
+echo Pull complete!
